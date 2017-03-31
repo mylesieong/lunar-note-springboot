b6a439dc776678a4bcf7237a382a4ba8fa12d61e
Myles Ieong Sai Hou <BI77@corp.dahsing.com>
2017-03-31 15:38:42 +0800

    Convert project to war package

--- src/main/java/com/bcm/app/App.java
+++ src/main/java/com/bcm/app/App.java
@@ -1,10 +1,17 @@
 package com.bcm.app;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer{
+    
+    @Override 
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(App.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
